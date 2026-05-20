edff18a326457dca3f2d196db7b30da7aebf9771
ryanjblajda <ryan.j.blajda@gmail.com>
2026-05-20 16:46:34 -0400

    feat: start implementing user oauth server

---
+++ Authentication/OpenAuth.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Duende.IdentityModel.OidcClient;
+using Duende.IdentityModel.OidcClient.Browser;
+using Newtonsoft.Json.Bson;
+using Serilog;
+
+namespace CommissioningChecklistGenerator.Authentication
+{
+    internal static class OpenAuth
+    {
+        private const string Prefix = "[OpenAuth]";
+
+        private const string RedirectUri = "http://localhost/callback";
+        private static OidcClientOptions OpenAuthConfiguration = new OidcClientOptions();
+        private static OidcClient OpenAuthClient = new OidcClient(OpenAuthConfiguration);
+
+        public static bool IsAuthenticated { get; private set; } = false;
+        public static bool IsInitialized { get; private set; } = false;
+
+        private static string Token = String.Empty;
+
+        public static void Initialize(string auth, string id)
+        {
+            OpenAuthConfiguration.Authority = auth;
+            OpenAuthConfiguration.ClientId = id;
+            OpenAuthConfiguration.Scope = "openid profile email";
+            OpenAuthConfiguration.RedirectUri = RedirectUri;
+            OpenAuthConfiguration.Browser = new SystemBrowser();
+            IsInitialized = true;
+        }
+
+        private static async Task Login()
+        {
+            if (IsInitialized) {
+                LoginResult result = await OpenAuthClient.LoginAsync();
+                Log.Debug($"{Prefix} login result -> {result}");
+            }
+            else { Log.Fatal($"{Prefix} open auth not initialized, cannot login"); }
+        }
+    }
+}
---
+++ Authentication/SystemBrowser.cs
@@ -0,0 +1,50 @@
+﻿using Duende.IdentityModel.OidcClient;
+using Duende.IdentityModel.OidcClient.Browser;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommissioningChecklistGenerator.Authentication
+{
+    internal class SystemBrowser : IBrowser
+    {
+        public bool IsRunning { get; private set; } = false;
+        public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default)
+        {
+            BrowserResult result = new BrowserResult
+            {
+                ResultType = BrowserResultType.UnknownError
+            };
+
+            if (!IsRunning)
+            {
+                HttpListener callbackListener = new HttpListener();
+
+                IsRunning = true;
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = options.StartUrl,
+                    UseShellExecute = true
+                });
+
+                HttpListenerContext context = await callbackListener.GetContextAsync();
+
+                result.ResultType = BrowserResultType.Success;
+            }
+            else 
+            { 
+                Log.Warning($"[{nameof(SystemBrowser)}] is already running, cannot invoke again until previous instance is closed"); 
+                result.Error = "Browser is already running, cannot invoke again until previous instance is closed";
+            }
+
+            return result;
+        }
+    }
+}
