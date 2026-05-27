a1c16d4808a8ed057f5afd020f1feccc6f7d2b71
ryanjblajda <ryan.j.blajda@gmail.com>
2026-05-27 15:05:29 -0400

    feat: open auth to only allow authenticated users

--- Authentication/OpenAuth.cs
+++ Authentication/OpenAuth.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
 using Duende.IdentityModel.OidcClient;
 using Duende.IdentityModel.OidcClient.Browser;
 using Newtonsoft.Json.Bson;
@@ -15,30 +16,45 @@ namespace CommissioningChecklistGenerator.Authentication
     {
         private const string Prefix = "[OpenAuth]";
 
-        private const string RedirectUri = "http://localhost/callback";
+        private const int RedirectPort = 7900;
+        private static readonly string RedirectUri = $"http://localhost:{RedirectPort}/";
         private static OidcClientOptions OpenAuthConfiguration = new OidcClientOptions();
-        private static OidcClient OpenAuthClient = new OidcClient(OpenAuthConfiguration);
+        private static OidcClient? OpenAuthClient;
 
         public static bool IsAuthenticated { get; private set; } = false;
-        public static bool IsInitialized { get; private set; } = false;
+
+        public static bool IsInitialized 
+        { 
+            get { return (OpenAuthClient != null); }
+        }
 
         private static string Token = String.Empty;
 
-        public static void Initialize(string auth, string id)
+        public static async Task Initialize(string auth, string id)
         {
-            OpenAuthConfiguration.Authority = auth;
-            OpenAuthConfiguration.ClientId = id;
-            OpenAuthConfiguration.Scope = "openid profile email";
-            OpenAuthConfiguration.RedirectUri = RedirectUri;
-            OpenAuthConfiguration.Browser = new SystemBrowser();
-            IsInitialized = true;
+            try { 
+                OpenAuthConfiguration.Authority = auth;
+                OpenAuthConfiguration.ClientId = id;
+                OpenAuthConfiguration.Scope = "openid profile email";
+                OpenAuthConfiguration.RedirectUri = RedirectUri;
+                OpenAuthConfiguration.Browser = new SystemBrowser();
+                OpenAuthConfiguration.DisablePushedAuthorization = true;
+
+                OpenAuthClient = new OidcClient(OpenAuthConfiguration);
+
+                await Login();
+            }
+            catch(Exception e) { Log.Fatal(e, $"{Prefix}");  }
         }
 
         private static async Task Login()
         {
             if (IsInitialized) {
-                LoginResult result = await OpenAuthClient.LoginAsync();
-                Log.Debug($"{Prefix} login result -> {result}");
+                LoginResult result = await OpenAuthClient.LoginAsync(new LoginRequest());
+
+
+
+                Log.Warning($"{Prefix} login result -> {result.Error} {result.ErrorDescription}");
             }
             else { Log.Fatal($"{Prefix} open auth not initialized, cannot login"); }
         }
--- Authentication/SystemBrowser.cs
+++ Authentication/SystemBrowser.cs
@@ -1,47 +1,108 @@
-﻿using Duende.IdentityModel.OidcClient;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Duende.IdentityModel.OidcClient;
 using Duende.IdentityModel.OidcClient.Browser;
 using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
+using System.Net.Mime;
+using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace CommissioningChecklistGenerator.Authentication
 {
     internal class SystemBrowser : IBrowser
     {
-        public bool IsRunning { get; private set; } = false;
+        private const string Prefix = "[SystemBrowser]";
+
+        private bool IsSuccessResponseCode(int code)
+        {
+            return (code >= 200 && code < 300);
+        }
+
+        private bool IsAuthenticated(NameValueCollection? parameters)
+        {
+            if (parameters != null)
+            {
+                return ((parameters["error"] == String.Empty) || (parameters["error"] == null));
+            }
+            return false;
+        }
+
+        private FileStream HandleRequest(Uri? url)
+        {
+            FileStream html = new FileStream("D:\\PERSONAL\\_ryan_root\\_projects\\_programming\\C#\\CommissioningChecklistGenerator\\Authentication\\failure.html", FileMode.Open, FileAccess.Read);
+
+            if (url != null)
+            { 
+                NameValueCollection parameters = HttpUtility.ParseQueryString(url?.Query ?? String.Empty);
+                
+                if (IsAuthenticated(parameters)) {
+                    html = new FileStream("D:\\PERSONAL\\_ryan_root\\_projects\\_programming\\C#\\CommissioningChecklistGenerator\\Authentication\\success.html", FileMode.Open, FileAccess.Read);
+                }
+                else { Log.Error($"{Prefix} {parameters["error"]} {parameters["error_description"]}"); }
+            }
+
+            return html;
+        }
+
         public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default)
         {
-            BrowserResult result = new BrowserResult
+            BrowserResult result = new BrowserResult { ResultType = BrowserResultType.UnknownError };
+            HttpListener? callbackListener = null;
+
+            try
             {
-                ResultType = BrowserResultType.UnknownError
-            };
-
-            if (!IsRunning)
-            {
-                HttpListener callbackListener = new HttpListener();
-
-                IsRunning = true;
-
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = options.StartUrl,
-                    UseShellExecute = true
-                });
-
-                HttpListenerContext context = await callbackListener.GetContextAsync();
-
-                result.ResultType = BrowserResultType.Success;
+                callbackListener = new HttpListener();
+                callbackListener.Prefixes.Add(options.EndUrl);
+                callbackListener.Start();
             }
-            else 
-            { 
-                Log.Warning($"[{nameof(SystemBrowser)}] is already running, cannot invoke again until previous instance is closed"); 
-                result.Error = "Browser is already running, cannot invoke again until previous instance is closed";
+            catch (Exception e)
+            {
+                Log.Fatal(e, $"{Prefix} creating http listener for server response callback");
+            }
+
+            if (callbackListener != null)
+            {
+                if (callbackListener.IsListening)
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = options.StartUrl,
+                        UseShellExecute = true
+                    });
+
+                    HttpListenerContext context = await callbackListener.GetContextAsync();
+
+                    FileStream reader = HandleRequest(context.Request.Url);
+
+                    context.Response.ContentType = "text/html; charset=utf-8";
+                    context.Response.ContentLength64 = reader.Length;
+
+                    await reader.CopyToAsync(context.Response.OutputStream);
+                    await context.Response.OutputStream.FlushAsync();
+                    context.Response.Close();
+
+                    result.Response = context.Request.Url?.AbsoluteUri ?? String.Empty;
+                    result.ResultType = IsAuthenticated(HttpUtility.ParseQueryString(context.Request.Url?.Query ?? String.Empty)) ? BrowserResultType.Success : BrowserResultType.HttpError;
+                }
+                else
+                {
+                    Log.Warning($"{Prefix} http callback not listening, could not handle server response");
+                    result.Error = "http callback not listening, could not handle server response";
+                }
+
+                callbackListener.Stop();
+                callbackListener.Close();
             }
 
             return result;
